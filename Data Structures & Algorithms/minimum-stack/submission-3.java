880724d8c9e073ac3fd25e451aae9d781186ef60
Pallav Kalal <48246740+pallav003@users.noreply.github.com>
2026-06-27 22:33:23 +0530

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.java
@@ -0,0 +1,30 @@
+class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || minStack.peek()>=val) {
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        int value = stack.pop();
+        if(minStack.peek() == value) {
+            minStack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
